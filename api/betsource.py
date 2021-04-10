6976da6c8e7f8ff4d838d304a20e300e3ac732fd
iameo <okwudike.emmanuel@gmail.com>
2021-04-10 17:46:21 +0100

    updated bet sources

--- api/betsource.py
+++ api/betsource.py
@@ -5,8 +5,17 @@ class BetSources(str, Enum):
     bet9ja = 'bet9ja'
     sportybet = 'sportybet'
     bet365 = 'bet365'
+    xbet = 'xbet' #1xbet
 
     @classmethod
     def list(cls):
         return list(map(lambda c: c.value, cls))
 
+
+
+
+############### LINKS ##############
+link_betway = ''
+link_bet9ja = 'https://web.bet9ja.com/Sport/Default.aspx'
+link_sportybet = 'https://www.sportybet.com/ng/sport/football/' #to evade homepage popups
+link_1xbet = 'https://1xbet.ng/en/line/' #to evade homepage popups but handles one
