ba712784d3203e81a1fb1e66b862551c416f14f7
iameo <okwudike.emmanuel@gmail.com>
2021-04-12 00:03:44 +0100

    index route now redirects to docs

--- api/main.py
+++ api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from .views.matches_view import match_view
 from .views.slips_view import slip_view
 
-app = FastAPI(title="BET CONVERTER")
+app = FastAPI(
+    title="BET CONVERTER",
+    description="API for converting a bet slip from one source to another!",
+    version="alpha")
 
+
+#Redirect to docs
+@app.get("/", tags=['Docs'])
+def docs():
+    return RedirectResponse('/docs')
 
 app.include_router(match_view,prefix="/api/v1/resources",tags=["Matches"])
 app.include_router(slip_view,prefix="/api/v1/resources",tags=["Slips"])
