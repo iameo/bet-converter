a5a7b12bfd5c1b41d4f9b3e616342fad808c7838
iameo <okwudike.emmanuel@gmail.com>
2021-04-27 19:49:17 +0100

    suitable window size in line with --headless for heroku

--- api/worker.py
+++ api/worker.py
@@ -53,7 +53,7 @@ class MatchExtractor(ABC):
         options = webdriver.ChromeOptions()
 
         options.add_argument("--headless")
-        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--window-size=1500,1000")
         options.add_argument('--disable-gpu')
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument('--no-sandbox')
