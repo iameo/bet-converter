8f44b7af4da147bb7c4cd65253d2958032137e15
iameo <okwudike.emmanuel@gmail.com>
2021-04-27 18:54:29 +0100

    timeout for db

--- api/database.py
+++ api/database.py
@@ -11,7 +11,7 @@ dotenv_path = join(dirname(__file__), ".env")
 load_dotenv(dotenv_path)
 
 engine = create_engine(
-    os.getenv('SQLAlCHEMY_DATABASE_URL', "sqlite:///./xxxxxx.db"), connect_args={"check_same_thread": False}
+    os.getenv('SQLAlCHEMY_DATABASE_URL', "sqlite:///./vt.db"), connect_args={"check_same_thread": False, "timeout": 15}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
