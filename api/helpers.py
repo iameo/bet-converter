ed03c5fbd8eac0b7793ca1c6f95ff692b3997497
iameo <okwudike.emmanuel@gmail.com>
2021-04-27 16:58:56 +0100

    helper functions here

---
+++ api/helpers.py
@@ -0,0 +1,27 @@
+import re
+from datetime import datetime
+import os
+import csv
+
+
+
+def log_error(status_error):
+    """
+    creates directory for storing logs for the day
+
+    return: None
+
+    """
+    today, now = datetime.today(), datetime.now()
+
+    
+    directory = 'errorlog'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    error_csv = str(today)+"_"+str(now.hour)+':'+str(now.minute)+':'+ str(now.second), status_error
+    error_file = open(str(directory)+'/betconv_exceptions'+str(directory)+'.csv', 'a+')
+    with error_file:
+        writer = csv.writer(error_file)
+        writer.writerow(error_csv)
+    return
