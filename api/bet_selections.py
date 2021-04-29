39b7f1073f9388a1e46bc8d4175ff7efde392fe2
iameo <okwudike.emmanuel@gmail.com>
2021-04-30 00:37:21 +0100

    fixed 1xbet to bet9ja

--- api/bet_selections.py
+++ api/bet_selections.py
@@ -11,24 +11,22 @@ def bet9ja_to_msport(bet):
 
 
 ###### SOURCE: 1XBET#######
-
-def x1bet_to_bet9ja(bet, team, league):
+# Southampton 1X2
+def x1bet_to_bet9ja(bet, home, away, league):
     bet_type = ''
     bet_selection = ''
-    home = team.split(' - ')[0].lower()
-    away = team.split(' - ')[1].lower()
 
     if 'qualify' in bet:
-        bet_type = bet.rsplit(' - ')[1]
-        bet_selection = bet.rsplit(' - ')[0]
+        bet_type = bet.rsplit(' - ')[0]
+        bet_selection = bet.rsplit(' - ')[1]
     elif 'total over' in bet:
-        bet_type = bet.split(' ', 1)[1]
-        bet_selection = bet.split(' ', 1)[0]
+        bet_type = bet.split(' ', 1)[0]
+        bet_selection = bet.split(' ', 1)[1]
     elif 'to win by (3way)' in bet:
-        bet_type = bet.split('.')[1]
-        bet_selection = bet.split('.')[0]
+        bet_type = bet.split('.')[0]
+        bet_selection = bet.split('.')[1]
     elif 'double chance' in bet:
-        bet_type = bet.split(' ', 2)[1]
+        bet_type = bet.split(' ', 2)[0]
         if home in bet_type and away in bet_type:
             bet_type = "12"
         elif home in bet_type:
@@ -38,21 +36,41 @@ def x1bet_to_bet9ja(bet, team, league):
         else:
             bet_type = ''
 
-        bet_selection = bet.split(' ', 2)[0]
+        bet_selection = bet.split(' ', 2)[1]
 
-    elif 'basketball' in league and '1x2' in bet:
+    elif ('basketball' in league or 'nba' in league) and '1 - 2' in bet:
         if home in bet_type:
             bet_type = "1HH"
         elif away in bet_type:
-            bet_type = 'X2'
+            bet_type = '2HH'
         else:
             bet_type = ''
-  
+    
+    elif ('basketball' in league or 'nba' in league) and ('1x2 rt' in bet or '1x2' in bet or '1x2 (5.5)' in bet):
+        if home in bet_type:
+            bet_type = "1"
+        elif away in bet_type:
+            bet_type = '2'
+        else:
+            bet_type = 'X'
+
+    elif ('basketball' in league or 'nba' in league) and ('1x2 in regular time' in bet):
+        if home in bet_type:
+            bet_type = "1"
+        elif away in bet_type:
+            bet_type = '2'
+        else:
+            bet_type = 'X'
 
     else:
-        bet_type = bet.split(' ')[1]
-        bet_selection = bet.split(' ')[0]
-
+        bet_type = bet.split(' ')[0]
+        bet_selection = bet.split(' ')[1]
+        if str(bet_type).lower() == str(home).lower():
+            bet_type = 1
+        elif str(bet_type).lower() == str(away).lower():
+            bet_type = 2
+        else:
+            bet_type = "X"
     return bet_type, bet_selection
     
 
