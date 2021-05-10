2758aec04a9fc5152c82df178bf73a3377de3af1
iameo <okwudike.emmanuel@gmail.com>
2021-05-10 22:26:28 +0100

    comment out; async db in use now

--- api/database.py
+++ api/database.py
@@ -1,19 +1,19 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy import create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+# import os
+# from os.path import join, dirname
+# from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), ".env")
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), ".env")
+# load_dotenv(dotenv_path)
 
-engine = create_engine(
-    os.getenv('SQLAlCHEMY_DATABASE_URL', "sqlite:///./vt.db"), connect_args={"check_same_thread": False, "timeout": 15}
-)
+# engine = create_engine(
+#     os.getenv('SQLAlCHEMY_DATABASE_URL', "sqlite:///./vt.db"), connect_args={"check_same_thread": False, "timeout": 15}
+# )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# Base = declarative_base()
