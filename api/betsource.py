17f5c9207f9dd38208dc8a62a35635ee1dacc9f2
iameo <okwudike.emmanuel@gmail.com>
2021-06-06 13:28:06 +0100

    betway link updated

--- api/betsource.py
+++ api/betsource.py
@@ -17,7 +17,7 @@ class BetSources(str, Enum):
 
 
 ############### LINKS ##############
-link_betway = ''
+link_betway = 'https://betway.com'
 link_bet9ja = 'https://web.bet9ja.com/Sport/Default.aspx'
 link_sportybet = 'https://www.sportybet.com/ng/sport/football/' #to evade homepage popups
 link_1xbet = 'https://1xbet.ng/en/line/' #to evade homepage popups but handles one
